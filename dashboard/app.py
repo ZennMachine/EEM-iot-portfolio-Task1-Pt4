931508d1fac88d07c8a0611c8e080f27d4149b2f
ZENNMACHINE <elijahmc@protonmail.com>
2021-12-04 23:03:25 +1100

    feature/provider: Basic API creted

--- dashboard/app.py
+++ dashboard/app.py
@@ -1,8 +1,53 @@
 from flask import Flask, render_template
 from db import Database
+import cpu_load
 
 app = Flask(__name__)
 
+@app.route("/api/cpu-load")
+def get_api_deviceload():
+    return {"cpu load": cpu_load.get_random_cpu_load,
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
+
+@app.route("/api/environment")
+def get_api_environment():
+    return {"error": "Route note implemented",
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
+@app.route("/api/temperature")
+def get_api_temperature():
+    return {"error": "Route note implemented",
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
+@app.route("/api/pressure")
+def get_api_pressure():
+    return {"error": "Route note implemented",
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
+@app.route("/api/humidity")
+def get_api_humidity():
+    return {"error": "Route note implemented",
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
+
+@app.route("/api/ROUTE")
+def get_api_ROUTE():
+    return {"error": "Route note implemented",
+    "temperature": None,
+    "pressure": None,
+    "humidity": None,
+    }
 
 @app.route('/')
 def index():
